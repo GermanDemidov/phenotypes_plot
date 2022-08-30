ef247fb66c2d84593cda15bad4551bafb4c7021a
AHDemiG1 <ahdemig1@SRV018.img.med.uni-tuebingen.de>
2022-08-30 15:52:01 +0200

    Added example plots

--- create_hpo_list_for_annotation.R
+++ create_hpo_list_for_annotation.R
@@ -1,10 +1,9 @@
 library(ontologyIndex)
 
-data_for_plotting <- read.table("/mnt/users/ahdemig1/forBenita/pheno_plot_22_07_27/outfile_annotated2_cp.txt", header=T, sep="\t", comment.char="&", fill=T)
 
-NGSD_database <- read.table("/mnt/users/ahdemig1/forJoo/NGSD150722_only_research.txt", header=T, comment.char = "?", sep="\t", fill = TRUE, quote = "\"")
+data_for_plotting <- read.table("/path_to_database/database.tsv", header=T, sep="\t", comment.char="&", fill=T, quote="\"")
 
-hpo <- get_ontology("/mnt/users/ahdemig1/forBenita/height_of_hpos/hp.obo")
+hpo <- get_ontology("/path_to_downloaded_files/hp.obo")
 
 blacklist_hpos = c("HP:0000006", "HP:0000007", "HP:0001417", "HP:0001419", "HP:0001423", "HP:0001428", "HP:0001450", "HP:0003593")
 
@@ -21,4 +20,4 @@ for (elem in set_of_branches) {
     table_to_output <- rbind(table_to_output, c(hpo_elem, name))
   }
 }
-write.table(table_to_output, file="./HPOs.disease.tsv", sep="\t", row.names = F, quote=F, col.names = F)
+write.table(table_to_output, file="/path_to_generated_with_create_hpo_list_for_annotationR/HPOs.disease.tsv", sep="\t", row.names = F, quote=F, col.names = F)
